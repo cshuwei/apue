d3e37713859fbe361729991a7ec1d69b84409240
csw <806695981@qq.com>
2017-12-21 06:29:38 -0800

    about mode

---
+++ file/access.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include "apue.h"
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        err_quit("usage: a.out <pahtname>");
+    }
+    if (access(argv[1], R_OK) < 0) {
+        err_ret("access error for %s", argv[1]);
+    } else {
+        printf("read acccess OK\n");
+    }
+
+    if (open(argv[1], O_RDONLY) < 0) {
+        err_ret("open error for %s", argv[1]);
+    } else {
+        printf("open for reading OK\n");
+    }
+    exit(0);
+}
